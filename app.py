7024bf2a0c3806242d99c9b615df19e896c7b6ce
Wassim EL BAKKOURI <62564480+wassim249@users.noreply.github.com>
2023-08-05 19:57:17 +0100

    add theme

--- app.py
+++ app.py
@@ -3,11 +3,20 @@ import streamlit as st
 from scripts.utils import build_prompt , load_model , predict
 
 
+# edit streamlit page config
+st.set_page_config(
+    page_title="RAZI.AI",
+    page_icon="👨🏼‍⚕️",
+    layout="centered",
+    initial_sidebar_state="auto",
+)
 
 st.title("👨🏼‍⚕️ RAZI.AI")
 st.markdown("This is a demo of a chatbot that helps you detect a mental illness. It is powered by OpenAI's GPT-3 API and a fine-tuned version of Google's Bert model.")
-st.markdown("Made by [Wassim EL BAKKOURI](#wassim.elbakkouri@yahoo.com) & (Fatima Zahra MOUMENE)(#moumene.fatimazahra2000@gmail.com)")
+st.markdown("Made by [Wassim EL BAKKOURI](#wassim.elbakkouri@yahoo.com) & [Fatima Zahra MOUMENE](#moumene.fatimazahra2000@gmail.com)")
 st.markdown('<style>' + open('./styles/style.css').read() + '</style>', unsafe_allow_html=True)
+
+
 
 # load the OpenAI API key
 openai.api_key = st.secrets["openai_secret"]
