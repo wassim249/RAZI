49c6c036570294b6a86c94cdb2f6c88a63a3a301
Wassim EL BAKKOURI <62564480+wassim249@users.noreply.github.com>
2023-08-05 12:12:14 +0100

    init commit

---
+++ app.py
@@ -0,0 +1,71 @@
+import openai
+import streamlit as st
+from scripts.utils import build_prompt , load_model , predict
+
+
+
+st.title("👨🏼‍⚕️ RAZI.AI")
+st.markdown("This is a demo of a chatbot that helps you detect a mental illness. It is powered by OpenAI's GPT-3 API and a fine-tuned version of Google's Bert model.")
+st.markdown("Made by [Wassim EL BAKKOURI](#wassim.elbakkouri@yahoo.com) & (Fatima Zahra MOUMENE)(#moumene.fatimazahra2000@gmail.com)")
+st.markdown('<style>' + open('/content/drive/MyDrive/RAZI/RAZI/styles/style.css').read() + '</style>', unsafe_allow_html=True)
+
+# load the OpenAI API key
+openai.api_key = st.secrets["openai_secret"]
+
+# load the model and tokenizer from the secrets
+model , tokenizer , label_encoder = load_model(
+    config_file = st.secrets["config_file"],
+    model_file = st.secrets["model_file"],
+    label_encoder_file = st.secrets["label_encoder_file"],
+    tokenizer_folder = st.secrets["tokenizer_folder"]
+    )
+
+def app():
+    # check if the user has selected a model
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    # display the model selection widget
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # display the messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # check if the user has entered a prompt
+    if prompt := st.chat_input("Feel free to describe your situation"):
+        # append the prompt to the messages
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # send the prompt to the API
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # send the response to the API
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            prediction , _ = predict(model,tokenizer,label_encoder,prompt)
+            prompt = build_prompt(prompt, prediction)
+
+            full_response = f"you are diagnosed with {prediction}\n\n"
+
+            # send the prompt to the API
+            for response in openai.ChatCompletion.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": prompt}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            ):
+
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        # append the full response to the messages
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
+if __name__ == "__main__":
+    app()
---
+++ scripts/utils.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+import numpy as np
+from transformers import BertTokenizer, TFBertForSequenceClassification , BertConfig
+from pickle import load
+
+def build_prompt(user_text,illness):
+    """
+    Build the prompt for the GPT-3 model
+    params:
+        user_text: string
+        illness: string
+    return:
+        prompt: string
+    """
+    return f"""
+    A person is diagnosed with {illness} and is experiencing the following symptoms:
+    {user_text}
+    recommend some tips to help them feel better using this format:
+    To feel better you need to :
+      1. Tip 1
+      2. Tip 2
+      ...
+    """
+
+def load_model(config_file , model_file , label_encoder_file, tokenizer_folder):
+    """
+    Load the BERT model and tokenizer
+    params:
+        config_file: string
+        model_file: string
+        label_encoder_file: string
+        tokenizer_folder: string
+    return:
+        model: TFBertForSequenceClassification
+        tokenizer: BertTokenizer
+        label_encoder: LabelEncoder
+    """
+    # Load the BERT model configuration
+    config = BertConfig.from_json_file(config_file)
+
+    # Load the BERT model weights
+    model = TFBertForSequenceClassification.from_pretrained(model_file, config=config)
+
+    # Load the tokenizer
+    tokenizer = BertTokenizer.from_pretrained(tokenizer_folder)
+
+    # Load the label encoder
+    with open(label_encoder_file, "rb") as f:
+        label_encoder = load(f)
+
+    return model , tokenizer , label_encoder
+
+def predict(bert_model , tokenizer ,lbl_encoder, input_text):
+    """
+    Predict the label of the input text
+    params:
+        bert_model: TFBertForSequenceClassification
+        tokenizer: BertTokenizer
+        lbl_encoder: LabelEncoder
+        input_text: string
+    return:
+        predicted_label: string
+        probabilities: numpy array
+    """
+    tokens = tokenizer.encode_plus(
+    input_text,
+    max_length=128,  # Adjust the max length as needed
+    padding="max_length",
+    truncation=True,
+    return_tensors="tf"
+    )
+    # Make predictions
+    outputs = bert_model(tokens.input_ids, attention_mask=tokens.attention_mask,)
+    logits = outputs.logits
+
+    # Apply softmax to obtain probabilities
+    probabilities = tf.nn.softmax(logits, axis=1)
+
+    # Convert probabilities to a NumPy array
+    probabilities = probabilities.numpy()
+
+    # Get the predicted label
+    predicted_label =lbl_encoder.inverse_transform( [int(np.argmax(probabilities, axis=1)[0])])
+
+    # Print the predicted label and probabilities
+    print("Predicted Label:", predicted_label)
+    print("Probabilities:", probabilities)
+
+    return predicted_label[0] , probabilities
+
+
+
+    
